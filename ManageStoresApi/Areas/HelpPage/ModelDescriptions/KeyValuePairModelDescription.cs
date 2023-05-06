15284f298d4f85e97353b715737bd64232d1be5d
Goldin Baloyi <GoldinB@hollywoodbets.net>
2023-05-06 19:50:39 +0200

    added more code structure

---
+++ ManageStoresApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace ManageStoresApi.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
