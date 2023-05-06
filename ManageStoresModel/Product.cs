5186011ae5e8ba841165acbbde4cf7f8aa848c65
Goldin Baloyi <GoldinB@hollywoodbets.net>
2023-05-06 20:04:35 +0200

    added product and store models

--- ManageStoresModel/Product.cs
+++ ManageStoresModel/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ManageStoresModel
 {
-    public class Class1
+    internal class Product
     {
     }
 }
--- ManageStoresModel/Store.cs
+++ ManageStoresModel/Store.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ManageStoresData
+namespace ManageStoresModel
 {
-    public class Class1
+    internal class Store
     {
     }
 }
