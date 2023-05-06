a7845d57b5900cc5a7577c0bd6d83aba2e9c54aa
Goldin Baloyi <GoldinB@hollywoodbets.net>
2023-05-06 19:15:01 +0200

    Add project files to Git

---
+++ ManageStores/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ManageStores
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
