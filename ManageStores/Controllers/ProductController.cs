a69b1a32c643cb8dbf0fa0d9e8ac8ad95eef375d
Goldin Baloyi <GoldinB@hollywoodbets.net>
2023-05-11 16:47:19 +0200

    added templates

--- ManageStores/Controllers/ProductController.cs
+++ ManageStores/Controllers/ProductController.cs
@@ -2,9 +2,11 @@
 using ManageStoresModel;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
 
 namespace ManageStores.Controllers
@@ -56,6 +58,27 @@ namespace ManageStores.Controllers
             }
 
             return RedirectToAction("AddProduct");
+        }
+
+        public FileResult DownloadTemplate(int Id)
+        {
+            switch (Id)
+            {
+                case 1:
+                    return File(System.IO.File.ReadAllBytes(HostingEnvironment.MapPath("~/Content/Templates/ProductCSVTemplate.csv"))
+                                , System.Net.Mime.MediaTypeNames.Application.Octet
+                                , Path.GetFileName(HostingEnvironment.MapPath("~/Content/Templates/ProductCSVTemplate.csv")));
+                case 2:
+                    return File(System.IO.File.ReadAllBytes(HostingEnvironment.MapPath("~/Content/Templates/ProductJsonTemplate.json"))
+                                , System.Net.Mime.MediaTypeNames.Application.Octet
+                                , Path.GetFileName(HostingEnvironment.MapPath("~/Content/Templates/ProductJsonTemplate.json")));
+                case 3:
+                    return File(System.IO.File.ReadAllBytes(HostingEnvironment.MapPath("~/Content/Templates/ProductXMLTemplate.xml"))
+                                , System.Net.Mime.MediaTypeNames.Application.Octet
+                                , Path.GetFileName(HostingEnvironment.MapPath("~/Content/Templates/ProductXMLTemplate.xml")));
+                    break;
+            }
+            return null;
         }
     }
 }
